42513dd3e6403d0631fa7c0c446c93393ab0e0fe
eight-123 <eito12323@gmail.com>
2026-03-25 18:11:58 +0900

    Update opcodes.js

--- opcodes.js
+++ opcodes.js
@@ -1,16 +1,18 @@
 window. OPCODES = {
   0x00:{m:"NOP",s:1},
+  0x02:{m:"ld (bc),a",s:1},
+  
 
-  0x3E:{m:"LD A,d8",s:2},
-  0x06:{m:"LD B,d8",s:2},
-  0x0E:{m:"LD C,d8",s:2}, 
-  0x26:{m:"LD H,d8",s:2},
-  0x2E:{m:"LD I,d8",s:2},
+  0x3E:{m:"ld A,d8",s:2},
+  0x06:{m:"ld B,d8",s:2},
+  0x0E:{m:"ld C,d8",s:2}, 
+  0x26:{m:"ld H,d8",s:2},
+  0x2E:{m:"ld I,d8",s:2},
 
   0xAF:{m:"XOR A",s:1},
 
-  0x32:{m:"LDD (HL),A",s:1},
-
+  0x32:{m:"ldd (HL),A",s:1},
+  0x01:{m:"ld bc,a16",s:3},
   0xCD:{m:"CALL a16",s:3},
   0xC3:{m:"JP a16",s:3},
 
@@ -18,8 +20,8 @@ window. OPCODES = {
   0x20:{m:"JR NZ,r8",s:2},
   0x28:{m:"JR Z,r8",s:2},
 
-  0xE0:{m:"LDH (a8),A",s:2},
-  0xF0:{m:"LDH A,(a8)",s:2},
+  0xE0:{m:"ld (ff00+(a8)),a",s:2},
+  0xF0:{m:"ld A,(ff00+(a8))",s:2},
 
   0xCB:{m:"PREFIX CB",s:1}
 };
